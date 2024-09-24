c4feb0526091761bc8228ce4f1c0324d1b9f82ff
leonb <leon.beeser@yahoo.de>
2024-09-24 19:26:44 +0200

    streamlined build

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
+mkdir -p -- "build/dist"
+
+cd "./build" || exit
+
+cp "../main.py" "./"
+cp "../config.json" "./dist"
+cp "../README.MD" "./dist"
+
 python -m venv zusi
-.\zusi\Scripts\activate
+./zusi/Scripts/activate
 pip3 install tqdm
 pip3 install pyinstaller
 cp "D:\SteamLibrary\steamapps\common\ZUSI 3 - Aerosoft Edition\64bit\ZusiFtdEditor.64.exe" "./"
 pyinstaller --onefile --icon=ZusiFtdEditor.64.exe main.py
-cp "config.json" "./dist"
-cp "README.MD" "./dist"
-rm -rf "./zusi"
+
+cd ".." || exit
+
+mkdir -p -- "./deploy"
+
+cp "./build/dist/main.exe" "./deploy/"
+cp "./build/dist/config.json" "./deploy/"
+cp "./build/dist/README.MD" "./deploy/"
+
 rm -rf "./build"
-rm -rf "main.spec"
-rm -rf "ZusiFtdEditor.64.exe"
