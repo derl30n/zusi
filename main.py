5be28bb082f289f9771178bc0790907834c9b41a
leonb <leon.beeser@yahoo.de>
2024-11-30 13:43:26 +0100

    reworked schedule processing

--- main.py
+++ main.py
@@ -3,8 +3,119 @@ import os
 import sqlite3
 import xml.etree.ElementTree as Et
 from dataclasses import dataclass, field
-from datetime import datetime
+from datetime import datetime, timedelta
 from tqdm import tqdm
+
+
+@dataclass(frozen=True)
+class ServiceEntry:
+    name: str
+    timeArr: datetime | None
+    timeDep: datetime | None
+    isTurnAround: bool
+    isPlannedStop: bool
+    runningDistance: int
+
+    def isValid(self) -> bool:
+        return all([self.timeDep, self.runningDistance])
+
+
+class ServiceData:
+    def __init__(self, schedule) -> None:
+        self.start: ServiceEntry = ServiceEntry("", None, None, False, False, 0)
+        self.end: ServiceEntry = ServiceEntry("", None, None, False, False, 0)
+        self.plannedStopps: list = []
+        self.turnarounds: int = 0
+        self.isValid: bool = False
+
+        self._constructService(schedule)
+
+    def _constructService(self, schedule) -> None:
+        entries = schedule.findall('FplZeile')
+
+        if not len(entries) > 0:
+            return
+
+        for entry in entries:
+            scheduleEntry = self._getScheduleEntryFromEntry(entry)
+
+            if not scheduleEntry.isValid():
+                continue
+
+            self.end = scheduleEntry
+
+            if not self.start.isValid():
+                self.start = scheduleEntry
+
+            if scheduleEntry.isPlannedStop:
+                if len(self.plannedStopps) > 0 and self.plannedStopps[-1].name == scheduleEntry.name:
+                    continue
+
+                self.plannedStopps.append(scheduleEntry)
+
+            # TODO: does a turnaround also need to be planned stop?
+            if scheduleEntry.isTurnAround:
+                self.turnarounds += 1
+
+        self.isValid = self.start.isValid() and self.end.isValid()
+
+    def _getScheduleEntryFromEntry(self, entry) -> ServiceEntry:
+        name = entry.findall('FplName')
+        arr = entry.findall('FplAnk')
+        dep = entry.findall('FplAbf')
+        isTA = entry.find('FplRichtungswechsel') is not None
+        isPS = all([name, arr, dep])
+        dist = entry.get('FplLaufweg')
+
+        nameStr = name[0].get('FplNameText') if len(name) > 0 else None
+        arrStr = self.getTime(arr[0].get('Ank')) if len(arr) > 0 else None
+        depStr = self.getTime(dep[0].get('Abf')) if len(dep) > 0 else None
+        distInt = int(float(dist)) if dist is not None else 0
+
+        return ServiceEntry(
+            nameStr,
+            arrStr,
+            depStr,
+            isTA,
+            isPS,
+            distInt
+        )
+
+    @staticmethod
+    def getTime(timeString: str | None) -> datetime | None:
+        if timeString is None:
+            return None
+
+        try:
+            return datetime.strptime(timeString, '%Y-%m-%d %H:%M:%S')
+        except ValueError:
+            return None
+
+    def getPlannedStopNamesAsList(self) -> list[str]:
+        return [entry.name for entry in self.plannedStopps]
+
+    def getStartTimeFormatted(self) -> str:
+        return datetime.strftime(self.start.timeDep, "%H:%M")
+
+    def getDuration(self) -> timedelta:
+        return self.end.timeDep - self.start.timeDep
+
+    def getAvgSpeed(self) -> int:
+        duration = self.getDuration().seconds
+
+        if duration == 0:
+            return 0
+
+        return int((self.end.runningDistance / duration) * 3.6)
+
+    def isStartAtPlatform(self) -> bool:
+        return self._isPointAtPlatform(self.start, 0)
+
+    def isEndAtPlatform(self) -> bool:
+        return self._isPointAtPlatform(self.end, -1)
+
+    def _isPointAtPlatform(self, point: ServiceEntry, index: int) -> bool:
+        return len(self.plannedStopps) > 0 and point.name == self.plannedStopps[index].name
 
 
 @dataclass(frozen=True)
@@ -56,68 +167,6 @@ def getTimetablesFromZusiFiles(config: Config) -> list:
     return timetables
 
 
-def getTimesFromTimetableEntry(zug, distance: int) -> (str, str):
-    start: str = ""
-    end: str = ""
-
-    for trn_type_tag in zug.findall('FahrplanEintrag'):
-        abf: str = trn_type_tag.get('Abf')
-
-        if not abf:
-            continue
-
-        if not start:
-            start = abf
-
-        end = abf
-
-    try:
-        start_time = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
-    except ValueError:
-        return "undefined", "undefined", 0
-
-    duration = "undefined"
-    dv = 0
-
-    try:
-        duration = datetime.strptime(end, '%Y-%m-%d %H:%M:%S') - start_time
-        dv = int((distance / duration.seconds) * 3.6)
-    except ValueError:
-        pass
-    except ZeroDivisionError:
-        pass
-
-    return datetime.strftime(start_time, "%H:%M"), str(duration), dv
-
-
-def getPlannedStoppsFromTimetable(timetable) -> (list, int):
-    stopps: list[str] = []
-    rw: int = 0
-
-    zeilen = timetable.findall('FplZeile')
-
-    for zeile in zeilen:
-
-        name = zeile.findall('FplName')
-        if not name:
-            continue
-
-        ank = zeile.findall('FplAnk')
-        if not ank:
-            continue
-
-        abf = zeile.findall('FplAbf')
-        if not abf:
-            continue
-
-        stopps.append(name[0].get('FplNameText'))
-
-        if zeile.find('FplRichtungswechsel') is not None:
-            rw += 1
-
-    return stopps, rw, int(float(zeilen[-1].get('FplLaufweg')))
-
-
 def isServiceValid(service: str, flagged_words: list[str]) -> bool:
     return not any([x.lower() in service.lower() for x in flagged_words])
 
@@ -138,27 +187,36 @@ def getDataFromTimetables(timetables: list, config: Config):
                 except FileNotFoundError:
                     continue
 
-                zug = root_trn.findall('Zug')[0]
-
-                if not isServiceValid(zug.get('FahrplanGruppe'), config.exclusionKeywords):
+                if not isServiceValid(root_trn.findall('Zug')[0].get('FahrplanGruppe'), config.exclusionKeywords):
                     continue
 
-                planned_stops, n_turnarounds, planned_distance = getPlannedStoppsFromTimetable(type_tag)
+                serviceData = ServiceData(type_tag)
 
-                # TODO: re-work
-                start_time, duration, dv = getTimesFromTimetableEntry(zug, planned_distance)
+                if not serviceData.isValid:
+                    print("service not valid!!", service)
+                    continue
+
+                planned_stops = serviceData.getPlannedStopNamesAsList()
+                n_turnarounds = serviceData.turnarounds
+                planned_distance = serviceData.end.runningDistance
+
+                start_time = serviceData.getStartTimeFormatted()
+                duration = str(serviceData.getDuration())
+                dv = serviceData.getAvgSpeed()
 
                 result.append(
                     {
                         "gattung": type_tag.get('Gattung'),
                         "zugnr": type_tag.get('Nummer'),
-                        "abfahrt": start_time,
+                        "begin": start_time,
                         "fahrzeit": duration,
                         "br": type_tag.get('BR'),
                         "laenge": int(float(type_tag.get('Laenge'))),
                         "masse": int(int(type_tag.get('Masse')) / 1000),
                         "nhalte": len(planned_stops),
-                        "nwendungen": n_turnarounds,
+                        "w1": n_turnarounds,
+                        "v3": serviceData.isStartAtPlatform(),
+                        "a3": serviceData.isEndAtPlatform(),
                         "s_km": int(planned_distance / 1000),
                         "dv": dv,
                         **getServiceInfo(service),
@@ -202,8 +260,8 @@ def createDatabaseWithData(data):
         pass
 
     cur.execute(
-        f"CREATE TABLE {table_name}(gattung, zugnr, abfahrt, fahrzeit, br, laenge, masse, nhalte, nwendungen, s_km, dv, country, route, fahrplan, zuglauf, halte)")
-    cur.executemany(f"INSERT INTO {table_name} VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
+        f"CREATE TABLE {table_name}(gattung, zugnr, begin, fahrzeit, br, laenge, masse, nhalte, w1, v3, a3, s_km, dv, country, route, fahrplan, zuglauf, halte)")
+    cur.executemany(f"INSERT INTO {table_name} VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
     con.commit()
 
     print("Zugdienste in Datenbank eingetragen.")
